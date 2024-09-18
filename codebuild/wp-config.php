43ec57aefc6336cb2b9f7261930aec7f0b9e8bec
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-19 01:44:36 +0530

    Create wp-config.php

---
+++ codebuild/wp-config.php
@@ -0,0 +1,14 @@
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress_db');
+
+/** MySQL database username */
+define('DB_USER', 'mydb');
+
+/** MySQL database password */
+define('DB_PASSWORD', '8SjhoTfVz2GAcADVJcor');
+
+/** MySQL hostname */
+define('DB_HOST', 'database-1.cr8mc00ii2ac.eu-north-1.rds.amazonaws.com');
+
+/**MYSQL PortNumber */
+define('DB_PORT', '3306');
