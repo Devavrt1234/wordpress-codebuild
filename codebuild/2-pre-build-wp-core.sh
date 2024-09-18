4c02dfe6d5c0ee836614c5f5742cf36f9c6903d0
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-19 01:28:01 +0530

    Update 2-pre-build-wp-core.sh

--- codebuild/2-pre-build-wp-core.sh
+++ codebuild/2-pre-build-wp-core.sh
@@ -1,9 +1,6 @@
 
 #!/bin/bash
-set -eo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-shopt -s nullglob # https://www.endpoint.com/blog/2016/12/12/bash-loop-wildcards-nullglob-failglob
-shopt -s nocasematch # Case Insensitive
-shopt -s expand_aliases # Script to check the alias output
+
 
 # Set working directory / alias
 cd $CODEBUILD_SRC_DIR/src
