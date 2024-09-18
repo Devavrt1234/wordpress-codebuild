2e4a464f39734ef825702ad6c42d74ad6f8614cf
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-19 00:36:35 +0530

    Update 2-pre-build-wp-core.sh

--- codebuild/2-pre-build-wp-core.sh
+++ codebuild/2-pre-build-wp-core.sh
@@ -9,12 +9,6 @@ cd $CODEBUILD_SRC_DIR/src
 alias wp='php wp-cli.phar --allow-root'
 echo "Running script: $(basename -- "$0")"
 
-echo "Retrieve values from AWS Secret Manager..."
-DB_HOST=$(aws secretsmanager get-secret-value --secret-id $SECRET_MANAGER_ARN --query 'SecretString' | jq -r 'fromjson | .DatabaseHost')
-DB_PORT=$(aws secretsmanager get-secret-value --secret-id $SECRET_MANAGER_ARN --query 'SecretString' | jq -r 'fromjson | .DatabasePort')
-DB_NAME=$(aws secretsmanager get-secret-value --secret-id $SECRET_MANAGER_ARN --query 'SecretString' | jq -r 'fromjson | .DatabaseName')
-DB_USERNAME=$(aws secretsmanager get-secret-value --secret-id $SECRET_MANAGER_ARN --query 'SecretString' | jq -r 'fromjson | .DatabaseUserName')
-DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id $SECRET_MANAGER_ARN --query 'SecretString' | jq -r 'fromjson | .DatabasePassword')
 
 echo "Installing WordPress CLI..."
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -23,16 +17,3 @@ chmod +x wp-cli.phar
 echo "Install Wordpress core files..."
 wp core download --locale=en_US --skip-content
 
-echo "Install WordPress Database Configurations..."
-wp config create --dbhost=$DB_HOST:$DB_PORT --dbname=$DB_NAME --dbuser=$DB_USERNAME --dbpass=$DB_PASSWORD
-
-if ! $( wp core is-installed ); then
-    echo "Install WordPress Database..."
-    # Change Options value
-    wp core install --url='http://example-elb-url.ap-southeast-1.elb.amazonaws.com/' \
-        --title='WordPress' \
-        --admin_user='admin' \
-        --admin_password='admin' \
-        --admin_email='admin@admin.com' \
-        --skip-email
-fi
